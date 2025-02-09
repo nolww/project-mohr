1c5d932f621f67f06342563c69dad6449a1a5b32
nouli <igornolasco2004@gmail.com>
2025-02-09 13:50:13 -0400

    Update SchedulerCatcher.ps1

--- SchedulerCatcher.ps1
+++ SchedulerCatcher.ps1
@@ -2,17 +2,17 @@ Start-Sleep -Seconds 1
 Write-Host "Scheduler Parser by nolw"
 Start-Sleep -Seconds 1
 
-# Obtém todas as tarefas agendadas e classifica as suspeitas
+
 $tasks = Get-ScheduledTask | ForEach-Object {
     $task = $_
     $actions = $task.Actions | ForEach-Object {
-        # Lista de executáveis potencialmente usados para bypass de segurança
+        
         $bypassPrograms = @(
             "cmd.exe", "powershell.exe", "powershell_ise.exe", "rundll32.exe", "regsvr32.exe", 
             "taskmgr.exe", "LaunchTM.exe", "WinRAR.exe"
         )
         
-        # Verifica se a ação está na lista de programas suspeitos
+        
         $suspect = if ($_.Execute -match ($bypassPrograms -join "|")) {
             "Yes"
         } else {
@@ -36,5 +36,5 @@ if ($tasks) {
     Write-Host "No tasks found"
 }
 
-# Mantém a janela aberta para evitar fechamento automático
+
 Read-Host "Press enter to quit"
